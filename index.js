4e35b155b25957155aceb8b7c5ed8460b6791948
burnettfr <burnettfr@gmail.com>
2024-01-08 08:35:43 -0500

    Update index.js and styles.css

--- index.js
+++ index.js
@@ -11,12 +11,16 @@ document.querySelector("#button1").addEventListener("click", handleClick);
 document.querySelector("#button2").addEventListener("click", startOver);
 
 function handleClick(){
+    buttonNumber = 1;
+    buttonAnimation(buttonNumber);
     correctAns = 0;
     /*alert("I got clicked");*/
     readAns();
 }
 
 function startOver(){
+    buttonNumber = 2;
+    buttonAnimation(buttonNumber);
     /*alert("Start Over");*/
     reset();
 }
@@ -25,48 +29,120 @@ function readAns(){
     for (var i = 0; i < numberOfQuest; i++) {
         if (i == 0 && document.querySelectorAll("select")[i].value == "C"){
             correctAns++;
+            document.querySelectorAll("select")[i].classList.remove("wrong");
+            document.querySelectorAll("select")[i].classList.add("correct");
+            document.querySelectorAll("select")[i].disabled = true
+        }
+        if (i == 0 && document.querySelectorAll("select")[i].value != "C"){
+            document.querySelectorAll("select")[i].classList.add("wrong");
         }
 
         if (i == 1 && document.querySelectorAll("select")[i].value == "I"){
             correctAns++;
+            document.querySelectorAll("select")[i].classList.remove("wrong");
+            document.querySelectorAll("select")[i].classList.add("correct");
+            document.querySelectorAll("select")[i].disabled = true
+        }
+        if (i == 1 && document.querySelectorAll("select")[i].value != "I"){
+            document.querySelectorAll("select")[i].classList.add("wrong");
         }
 
         if (i == 2 && document.querySelectorAll("select")[i].value == "L"){
             correctAns++;
+            document.querySelectorAll("select")[i].classList.remove("wrong");
+            document.querySelectorAll("select")[i].classList.add("correct");
+            document.querySelectorAll("select")[i].disabled = true
+        }
+        if (i == 2 && document.querySelectorAll("select")[i].value != "L"){
+            document.querySelectorAll("select")[i].classList.add("wrong");
         }
 
         if (i == 3 && document.querySelectorAll("select")[i].value == "J"){
             correctAns++;
+            document.querySelectorAll("select")[i].classList.remove("wrong");
+            document.querySelectorAll("select")[i].classList.add("correct");
+            document.querySelectorAll("select")[i].disabled = true
+        }
+        if (i == 3 && document.querySelectorAll("select")[i].value != "J"){
+            document.querySelectorAll("select")[i].classList.add("wrong");
         }
 
         if (i == 4 && document.querySelectorAll("select")[i].value == "E"){
             correctAns++;
+            document.querySelectorAll("select")[i].classList.remove("wrong");
+            document.querySelectorAll("select")[i].classList.add("correct");
+            document.querySelectorAll("select")[i].disabled = true
+        }
+        if (i == 4 && document.querySelectorAll("select")[i].value != "E"){
+            document.querySelectorAll("select")[i].classList.add("wrong");
         }
 
         if (i == 5 && document.querySelectorAll("select")[i].value == "G"){
             correctAns++;
+            document.querySelectorAll("select")[i].classList.remove("wrong");
+            document.querySelectorAll("select")[i].classList.add("correct");
+            document.querySelectorAll("select")[i].disabled = true
+        }
+        if (i == 5 && document.querySelectorAll("select")[i].value != "G"){
+            document.querySelectorAll("select")[i].classList.add("wrong");
         }
 
         if (i == 6 && document.querySelectorAll("select")[i].value == "B"){
             correctAns++;
+            document.querySelectorAll("select")[i].classList.remove("wrong");
+            document.querySelectorAll("select")[i].classList.add("correct");
+            document.querySelectorAll("select")[i].disabled = true
+        }
+        if (i == 6 && document.querySelectorAll("select")[i].value != "B"){
+            document.querySelectorAll("select")[i].classList.add("wrong");
         }
 
         if (i == 7 && document.querySelectorAll("select")[i].value == "K"){
             correctAns++;
+            document.querySelectorAll("select")[i].classList.remove("wrong");
+            document.querySelectorAll("select")[i].classList.add("correct");
+            document.querySelectorAll("select")[i].disabled = true
+        }
+        if (i == 7 && document.querySelectorAll("select")[i].value != "K"){
+            document.querySelectorAll("select")[i].classList.add("wrong");
         }
 
         if (i == 8 && document.querySelectorAll("select")[i].value == "D"){
             correctAns++;
+            document.querySelectorAll("select")[i].classList.remove("wrong");
+            document.querySelectorAll("select")[i].classList.add("correct");
+            document.querySelectorAll("select")[i].disabled = true
+        }
+        if (i == 8 && document.querySelectorAll("select")[i].value != "D"){
+            document.querySelectorAll("select")[i].classList.add("wrong");
         }
     }
     document.querySelectorAll("#textChange")[0].innerText = "Number Correct: " + correctAns;
-    console.log(correctAns);
+    /*console.log(correctAns);*/
 }
 
 function reset(){
     for (var i = 0; i < numberOfQuest; i++) {
         document.querySelectorAll("select")[i].value = " "
+        document.querySelectorAll("select")[i].classList.remove("wrong");
+        document.querySelectorAll("select")[i].classList.remove("correct");
+        document.querySelectorAll("select")[i].disabled = false
     }
 
     document.querySelectorAll("#textChange")[0].innerText = "Number Correct:";
 }
+
+function buttonAnimation(buttonNumber) {
+    /*alert("Button Animation");*/
+    console.log(buttonNumber);
+    
+    var activeButton = document.querySelector("#button" + buttonNumber);
+  
+    activeButton.classList.add("pressed");
+  
+    setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    }, 100);
+    
+  }
+
