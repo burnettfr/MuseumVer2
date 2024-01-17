6754bf127f75660123e81176b271f8dea2a06637
burnettfr <burnettfr@gmail.com>
2024-01-17 15:38:34 -0500

    Update pop up screen

--- index.js
+++ index.js
@@ -121,26 +121,9 @@ function readAns(){
     inCorrectAns = 9 - correctAns;
     /*console.log(correctAns);*/
 
-    
+    popUp(correctAns, inCorrectAns);
 
-    var myWindow = window.open("", "", "width=500,height=500, top=400, left=400");
-
-    myWindow.document.write("<style>html{background-color: green}</style>");
-    // myWindow.document.write("<style>h1{text-align: center}</style>");
-
-    myWindow.document.write("<h1 style = 'text-align: center'>Good News</h1>");
-    myWindow.document.write("<h1 style = 'text-align: center'>Lots of Opportunity to Improve</h1>");
-    myWindow.document.write("<h1 style = 'text-align: center; font-size: 10rem; margin: 0px'>&#128169</h1>");
-    myWindow.document.write("<h1 style = 'text-align: center; margin-top: 40px'>Number Correct: ");
-    myWindow.document.write(correctAns);
-    myWindow.document.write("</h1>");
-    myWindow.document.write("<h1 style = 'text-align: center'>Number Incorrect: ");
-    myWindow.document.write(inCorrectAns);
-    myWindow.document.write("</h1>");
-    
-    // var myWindow = window.open("popup1.html", "", "width=400,height=400, top=400, left=400");
-    // myWindow.document.write("<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>");
-}
+    }
 
 function reset(){
     for (var i = 0; i < numberOfQuest; i++) {
@@ -166,4 +149,26 @@ function buttonAnimation(buttonNumber) {
     }, 100);
     
   }
+
+  function popUp(correct, incorrect){
+    var myWindow = window.open("", "", "width=500,height=600, top=300, left=400");
+    // var myWindow = window.open("", "_parent");
+
+    myWindow.document.write("<style>html{background-color: green}</style>");
+
+    myWindow.document.write("<h1 style = 'text-align: center'>Good News</h1>");
+    myWindow.document.write("<h1 style = 'text-align: center'>Lots of Opportunity to Improve</h1>");
+    
+    myWindow.document.write("<h1 style = 'text-align: center; font-size: 10rem; margin: 0px'>&#128169</h1>");
+    
+    myWindow.document.write("<h1 style = 'text-align: center; margin-top: 40px'>Number Correct: ");
+    myWindow.document.write(correct);
+    myWindow.document.write("</h1>");
+    
+    myWindow.document.write("<h1 style = 'text-align: center'>Number Incorrect: ");
+    myWindow.document.write(incorrect);
+    myWindow.document.write("</h1>");
+
+    myWindow.document.write("<button onclick='window.close()' style = 'margin-left: 120px; font-family: serif; color: black; font-size: 2.5rem; border-radius: 25px;'>Close Window</button>");
+ }
 
